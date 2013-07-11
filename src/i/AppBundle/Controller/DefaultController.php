5988498457435a51c78cb67e0453aa4d8b171fe9
Max Gribov <max@neuropunks.org>
2013-07-11 19:52:58 -0400

    adding basic routes and templates

--- src/i/AppBundle/Controller/DefaultController.php
+++ src/i/AppBundle/Controller/DefaultController.php
@@ -3,11 +3,43 @@
 namespace i\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use JMS\SecurityExtraBundle\Annotation\Secure;
 
-class DefaultController extends Controller
-{
-    public function indexAction($name)
-    {
-        return $this->render('iAppBundle:Default:index.html.twig', array('name' => $name));
+class DefaultController extends Controller {
+    
+    /** 
+     * @Route("/login", name="login")
+     * @Template()
+     */    
+    public function loginAction() {
+        return $this->render('iAppBundle:Default:login.html.twig');
     }
+
+    /** 
+     * @Route("/upload", name="upload")
+     * @Template()
+     * @Secure(roles="ROLE_USER")
+     */    
+    public function uploadAction() {
+        return $this->render('iAppBundle:Default:upload.html.twig');
+    }
+
+    /** 
+     * @Route("/pic/{id}/like", name="like")
+     * @Template()
+     */    
+    public function likeAction($id) {
+ 
+    }
+
+    /** 
+     * @Route("/{page}", name="index", defaults={"page" = 1}, requirements={"page" = "\d+"})
+     * @Template()
+     */                
+    public function indexAction($page = 1) {
+        return $this->render('iAppBundle:Default:index.html.twig');
+    }
+    
 }
