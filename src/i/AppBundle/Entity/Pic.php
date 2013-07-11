e35cd333061599d5be4439947013d8873f4a33ea
Max Gribov <max@neuropunks.org>
2013-07-11 19:14:15 -0400

    adding create timestamp to Pic

--- src/i/AppBundle/Entity/Pic.php
+++ src/i/AppBundle/Entity/Pic.php
@@ -136,4 +136,32 @@ class Pic
     {
         return $this->likes;
     }
+    /**
+     * @var \DateTime
+     */
+    private $createdAt;
+
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Pic
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
 }
